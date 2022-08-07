a4da2f7e7f4354f110e47bbe7c3728c34a4b8c40
Merthan Kavak <merthan.kavak@gmail.com>
2022-08-07 17:10:32 +0300

    Delete lib/product/mixin directory

--- lib/product/mixin/no_network_mixin.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-mixin NoNetworkMixin {
-  void waitForScreen(VoidCallback onComplete) {
-    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-      onComplete.call();
-    });
-  }
-}
